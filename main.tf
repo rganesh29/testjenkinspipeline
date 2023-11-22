b3199d860388790ea1e26b4828d37ba3dc208a63
rganesh29 <ganeshr2903@gmail.com>
2023-11-22 08:11:29 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = var.region
 }
-ubuntu@ip-172-31-42-222:~/terraform$ cat main.tf 
+ 
 resource "aws_instance" "logic1" {
   ami           = var.ami_id
   instance_type = var.instance_type
