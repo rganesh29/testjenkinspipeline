0b3f7eb19d335f1c52bc45839abf0e5534059750
rganesh29 <151710631+rganesh29@users.noreply.github.com>
2023-12-12 11:36:05 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bucketforstatefile"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"  # Replace with your bucket's region
-  }
-}
